001eccf9745259222f59b2c043e9c73baf3d695d
crystalmenthol <wutwut@odds.team>
2023-11-25 15:04:07 +0700

    command pattern

--- code/src/command/lab/controller.test.ts
+++ code/src/command/lab/controller.test.ts
@@ -1,6 +1,13 @@
+import {
+  GarageDoorDownCommand,
+  GarageDoorLightOffCommand,
+  GarageDoorLightOnCommand,
+  GarageDoorStopCommand,
+  GarageDoorUpCommand,
+} from './command/garageDoorCommand';
 import { LightOffCommand, LightOnCommand } from './command/lightCommand';
 import { Controller } from './controller';
-import { Light } from './devices';
+import { GarageDoor, Light } from './devices';
 
 describe('[Command - lab] Controller', () => {
   it('should execute device that wrapped with command object', () => {
@@ -16,5 +23,35 @@ describe('[Command - lab] Controller', () => {
 
     controller.execute(lightOffCommand);
     expect(light.off).toHaveBeenCalledTimes(1);
+  });
+
+  it('should execute garage door that wrapped with command object', () => {
+    const controller = new Controller();
+    const garageDoor = new GarageDoor();
+    garageDoor.up = jest.fn();
+    garageDoor.down = jest.fn();
+    garageDoor.stop = jest.fn();
+    garageDoor.lightOn = jest.fn();
+    garageDoor.lightOff = jest.fn();
+    const garageDoorUpCommand = new GarageDoorUpCommand(garageDoor);
+    const garageDoorDownCommand = new GarageDoorDownCommand(garageDoor);
+    const garageDoorStopCommand = new GarageDoorStopCommand(garageDoor);
+    const garageDoorLightOnCommand = new GarageDoorLightOnCommand(garageDoor);
+    const garageDoorLightOffCommand = new GarageDoorLightOffCommand(garageDoor);
+
+    controller.execute(garageDoorUpCommand);
+    expect(garageDoor.up).toHaveBeenCalledTimes(1);
+
+    controller.execute(garageDoorDownCommand);
+    expect(garageDoor.down).toHaveBeenCalledTimes(1);
+
+    controller.execute(garageDoorStopCommand);
+    expect(garageDoor.stop).toHaveBeenCalledTimes(1);
+
+    controller.execute(garageDoorLightOnCommand);
+    expect(garageDoor.lightOn).toHaveBeenCalledTimes(1);
+
+    controller.execute(garageDoorLightOffCommand);
+    expect(garageDoor.lightOff).toHaveBeenCalledTimes(1);
   });
 });
